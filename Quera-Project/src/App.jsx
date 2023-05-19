2fd44e98a5a0c8efca539aebcd0c62b9be4bcf4e
m2y2m <meysamsayyadtala@gmail.com>
2023-05-19 08:42:02 +0430

    App.jsx matched

--- Quera-Project/src/App.jsx
+++ Quera-Project/src/App.jsx
@@ -1,11 +1,9 @@
-
 import './App.css';
 import { Routes, Route} from "react-router-dom";
-// import { Login } from './Pages/Login/Login';
-// import {Forget} from "./Pages/Forget/Forget";
-// import {Reset} from "./Pages/Reset/Reset";
+import { Login } from './Pages/Login/Login';
+import {Forget} from "./Pages/Forget/Forget";
+import {Reset} from "./Pages/Reset/Reset";
 import {Register} from "./Pages/Register/Register";
-
 
 function App() {
 
@@ -16,11 +14,13 @@ function App() {
       <div >
     
       <Routes >
-        
+          
           <Route path='/' element={<Register />}  />
-          {/* <Route path="Forget" element={<Forget />} />
-          <Route path="Reset" element={<Reset />} /> 
-           <Route path="Login" element={<Login />} /> */}
+          <Route path="/" element={<Forget />} />
+          <Route path="/" element={<Reset />} /> 
+          <Route path="/" element={<Login />} />
+        
+
       </Routes>
    
     
