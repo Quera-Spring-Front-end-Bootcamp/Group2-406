e022973fb0664ad62ee9acb4ab336e1dc72bc4b4
m2y2m <meysamsayyadtala@gmail.com>
2023-05-25 16:20:06 +0330

    minor fixes

--- Quera-Project/src/components/FIlterDropdown/ChooseFilter.jsx
+++ Quera-Project/src/components/FIlterDropdown/ChooseFilter.jsx
@@ -3,7 +3,8 @@ import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import { useState } from "react";
 import { ChooseDecide } from "./ChooseDecide"
 import { ChooseTag} from "./ChooseTag";
-export const ChooseFilter = ({ tag, type, decide, selectDecide,id }) => {
+import { ChooseType } from "./ChooseType";
+export const ChooseFilter = ({ tag, type, decide, selectDecide,id,selectTag,selectType,DeleteFilter }) => {
   const [showTag, setShowTag] = useState(false);
   const [showType, setShowType] = useState(false);
   const [showDecide, setShowDecide] = useState(false);
@@ -11,21 +12,28 @@ export const ChooseFilter = ({ tag, type, decide, selectDecide,id }) => {
     <article className="flex mb-6 flex-row items-center gap-[71px]">
       <article className="flex flex-row items-center  gap-[10px]">
         <p className=" text-sm">تسک‌هایی که</p>
-        <button className="w-[182px] border px-2 py-1 rounded-md border-[#E9EBF0] justify-between  text-xs items-center flex flex-row">
-          <p style={{ color: tag == "انتخاب کنید" ? "#959595" : "black" }}>
+        <article  className="flex flex-col gap-1 mt-1   justify-center items-center">
+        <button tabIndex="0"
+          onBlur={()=>{setShowType(false)}}
+            onClick={() => {
+              setShowType(!showType);
+            }} className="w-[182px] border px-2 py-1 rounded-md border-[#E9EBF0] justify-between  text-xs items-center flex flex-row">
+          <p style={{ color: type == "انتخاب کنید" ? "#959595" : "black" }}>
             {type}
           </p>
           <KeyboardArrowDownIcon className=" !text-sm" />
         </button>
+        <ChooseType showType={showType} id={id} selectType={selectType} setShowType={setShowType}/>
+        </article>
         <p className=" text-sm">آن‌ها</p>
         <article  className="flex flex-col gap-1 mt-1   justify-center items-center">
         <button tabIndex="0" onBlur={()=>{setShowTag(false)}} onClick={()=>{setShowTag(!showTag)}} className="w-[146px] text-xs items-center px-2 py-1 rounded-md border border-[#E9EBF0] justify-between flex flex-row">
-          <p style={{ color: type == "انتخاب کنید" ? "#959595" : "black" }}>
+          <p className=" rounded-md " style={{padding: tag == "انتخاب کنید" ? "0":"2px", color: tag == "انتخاب کنید" ? "#959595" : "black",backgroundColor: tag=="درس" ? "#EBC8C8":tag=="کار" ? "#C3B7F2": tag=="پروژه" ? "#7FFAFA":"transparent" }}>
             {tag}
           </p>
           <KeyboardArrowDownIcon className=" !text-sm" />
         </button>
-        <ChooseTag showTag={showTag} id={id} setShowTag={setShowTag}/></article>
+        <ChooseTag showTag={showTag} id={id} selectTag={selectTag} setShowTag={setShowTag}/></article>
         <article  tabIndex="0"
           onBlur={()=>{setShowDecide(false)}}
             onClick={() => {
@@ -48,7 +56,7 @@ export const ChooseFilter = ({ tag, type, decide, selectDecide,id }) => {
         </article>
       </article>
       <article>
-        <DeleteOutlineOutlinedIcon />
+        <DeleteOutlineOutlinedIcon onClick={()=>{DeleteFilter(id)}} className=" cursor-pointer" />
       </article>
     </article>
   );
--- Quera-Project/src/components/FIlterDropdown/ChooseTag.jsx
+++ Quera-Project/src/components/FIlterDropdown/ChooseTag.jsx
@@ -1,6 +1,6 @@
 import SearchIcon from '@mui/icons-material/Search';
 import { useRef } from 'react';
-export const ChooseTag=({showTag,setShowTag,id})=>{
+export const ChooseTag=({showTag,setShowTag,id,selectTag})=>{
   const input=useRef()
   return  <div className="relative  flex flex-col items-center">
     <span className="absolute">
@@ -20,7 +20,7 @@ export const ChooseTag=({showTag,setShowTag,id})=>{
         <li
           onClick={() => {
             setShowTag(false);
-           
+            selectTag(id,"درس")
           }}
           className=" mx-2 rounded-md text-center flex justify-center  py-auto mb-[12px] w-[44px] h-[31px] bg-[#EBC8C8]  cursor-pointer hover:opacity-70"
         >
@@ -28,14 +28,14 @@ export const ChooseTag=({showTag,setShowTag,id})=>{
         </li>
         <li  onClick={() => {
             setShowTag(false);
-           
+            selectTag(id,"کار")
           }}
           className=" mx-2 rounded-md text-center flex justify-center  py-auto mb-[12px] w-[31px] h-[31px] bg-[#C3B7F2]  cursor-pointer hover:opacity-70"
         >
          <p className='flex justify-center items-center'> کار</p></li>
          <li  onClick={() => {
             setShowTag(false);
-           
+            selectTag(id,"پروژه")
           }}
           className=" mx-2 rounded-md text-center flex justify-center  py-auto mb-[12px] w-[44px] h-[31px] bg-[#7FFAFA]  cursor-pointer hover:opacity-70"
         >
