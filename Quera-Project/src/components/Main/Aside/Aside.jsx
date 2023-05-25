97078f1376074183227e7d6dbc54bf99403748c4
selma <selma.charghadchi@gmal.com>
2023-05-25 18:51:12 +0330

    react-draggable is added

--- Quera-Project/src/components/Main/Aside/Aside.jsx
+++ Quera-Project/src/components/Main/Aside/Aside.jsx
@@ -2,7 +2,7 @@
 export const Aside = () => {
 
     return(
-        <aside className='w-[330px] border-solid border-l-2 border-gray-200 h-[1024px]'>
+        <aside className='bg-white w-[340px] border-solid border-l-2 border-gray-200 h-[1024px] z-30'>
             {/* Quera task manager name */}
             <div className="ml-16 mt-14 w-100 bg-namegradient bg-clip-text text-transparent text-4xl font-dana font-extrabold tracking-tight text-headerSize">کوئرا تسک منیجر</div>
             {/*  */}
