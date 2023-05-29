38b7a1be9bbd7ce70e1084c4611090b346ee65c8
Nastaran Azizi <aziizii.nastaran@gmail.com>
2023-05-29 09:36:03 +0330

    dropdown processName

--- Quera-Project/src/components/Main/MainComponent/ProjectsBoard/Pending.jsx
+++ Quera-Project/src/components/Main/MainComponent/ProjectsBoard/Pending.jsx
@@ -19,14 +19,12 @@ export const Pending = ({
 }) => {
   const [showTask, setShowTask] = useState(false);
 
-  function taskHandler() {
-    setShowTask(true);
-  }
+
   return (
     <>
       <section className="w-full flex flex-row-reverse  justify-between  mt-7  font-dana">
         <article className="w-1/2 flex  items-center  flex-row-reverse  mr-10">
-          <span className="flex" onClick={taskHandler}>
+          <span className="flex" onClick={()=>{setShowTask(!showTask)}}>
             {Icon}
           </span>
           <span className="text-xl  p-1 mr-1  bg-pink-500 rounded text-white text tracking-wider">
