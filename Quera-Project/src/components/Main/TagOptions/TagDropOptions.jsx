f323efa59881203e91539322f67d3b8e4d8abb24
m2y2m <meysamsayyadtala@gmail.com>
2023-06-07 08:29:37 +0330

    some codes formatted

--- Quera-Project/src/components/Main/TagOptions/TagDropOptions.jsx
+++ Quera-Project/src/components/Main/TagOptions/TagDropOptions.jsx
@@ -1,22 +1,46 @@
-import CloseIcon from '@mui/icons-material/Close';
-import EditIcon from '@mui/icons-material/Edit';
-import ColorLensOutlinedIcon from '@mui/icons-material/ColorLensOutlined';
-import { Tagcolors } from './Tagcolors';
-import { useState } from 'react';
-export const Tagdropoptions=({tagOptions,dropref,id})=>{
-  const [showcolors,setcolors]=useState(false)
-return <article     dir='rtl' style={{display:tagOptions ? "flex":"none"}} ref={dropref} className=" cursor-default z-10 h-[85px] justify-between min-w-[90px] rounded-lg shadow-md absolute bg-white p-2 font-dana flex-col">
-    <section className='flex cursor-pointer flex-row  gap-1'>
-      <CloseIcon className='!text-xs'/><small className=' text-[10px]'>حذف</small>
-    </section>
-    <section className='flex flex-row gap-1'>
-        <EditIcon className=' !text-xs text-[#BDBDBD]'/><small className=' cursor-pointer text-[10px]'>ویرایش تگ</small>
-        </section>
-        <section onMouseLeave={()=>{setcolors(false)}}   className='flex cursor-pointer flex-row gap-1'>
-        <span id={id}  onClick={()=>{setcolors(!showcolors)}}><ColorLensOutlinedIcon className=' !text-xs text-[#BDBDBD]'/><small id={id} className=' text-[10px]'>ویرایش رنگ</small></span>
-        <span className='relative'><Tagcolors showcolors={showcolors}/></span>
-        </section>
-</article>
-
-
-}
+import CloseIcon from "@mui/icons-material/Close";
+import EditIcon from "@mui/icons-material/Edit";
+import ColorLensOutlinedIcon from "@mui/icons-material/ColorLensOutlined";
+import { Tagcolors } from "./Tagcolors";
+import { useState } from "react";
+export const Tagdropoptions = ({ tagOptions, dropref, id }) => {
+  const [showcolors, setcolors] = useState(false);
+  return (
+    <article
+      dir="rtl"
+      style={{ display: tagOptions ? "flex" : "none" }}
+      ref={dropref}
+      className=" cursor-default z-10 h-[85px] justify-between min-w-[90px] rounded-lg shadow-md absolute bg-white p-2 font-dana flex-col"
+    >
+      <section className="flex cursor-pointer flex-row  gap-1">
+        <CloseIcon className="!text-xs" />
+        <small className=" text-[10px]">حذف</small>
+      </section>
+      <section className="flex flex-row gap-1">
+        <EditIcon className=" !text-xs text-[#BDBDBD]" />
+        <small className=" cursor-pointer text-[10px]">ویرایش تگ</small>
+      </section>
+      <section
+        onMouseLeave={() => {
+          setcolors(false);
+        }}
+        className="flex cursor-pointer flex-row gap-1"
+      >
+        <span
+          id={id}
+          onClick={() => {
+            setcolors(!showcolors);
+          }}
+        >
+          <ColorLensOutlinedIcon className=" !text-xs text-[#BDBDBD]" />
+          <small id={id} className=" text-[10px]">
+            ویرایش رنگ
+          </small>
+        </span>
+        <span className="relative">
+          <Tagcolors showcolors={showcolors} />
+        </span>
+      </section>
+    </article>
+  );
+};
